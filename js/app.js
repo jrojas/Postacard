ef476d27ad68696130d1bc18fb23adb2aee0b6d8
Jorge Rojas <jorge.l.rojas@gmail.com>
2014-02-02 23:02:56 -0800

    Revert "Revert "inital commit""
    This reverts commit 1aee6ebda3d816eae274e94be613affd2154f7fb.

---
+++ js/app.js
@@ -0,0 +1,55 @@
+var postaCard = (function() {
+      
+   
+
+    // public variables for PostaCard
+   
+
+    
+    // public init method to be exposed to the document ready function 
+    var init = function(){
+        
+        
+     };
+    
+    var searchPhoto = function(tag){
+        
+        console.log(tag);
+        var url= "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=?&amp;client_id=d57bbd445f2e4d4685f8f3710ef9a394"
+        $.getJSON(url, viewData);
+           
+    
+    };
+    
+    
+    var viewData = function(photos){
+        
+        console.log(photos);
+        $.each(photos.data, function(index, photo){
+            photo = "<img src='" +photo.images.low_resolution.url +"'/>";
+            $('.instagramPhotos').append(photo);
+        
+        });
+           
+    
+    };
+    
+
+
+    // public API
+    return {loadProp: init,
+            search: searchPhoto
+           };
+ 
+})();
+
+
+$(document).ready(function()
+{
+    postaCard.search('santodomingo');
+    
+ 
+});
+     
+
+ 

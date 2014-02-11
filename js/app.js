e469395c78ee510473fbe663507e508c2b787c28
jrojas <jorge.l.rojas@gmail.com>
2014-02-11 13:53:07 -0800

    handlebars

--- js/app.js
+++ js/app.js
@@ -1,51 +1,79 @@
 var postaCard = (function() {
-      
-   
-
     // public variables for PostaCard
- 
 
     // public init method to be exposed to the document ready function 
     var init = function(){
-        
-        searchPhoto();
-        
+ 
+        searchPhotoButton();
+
      };
     
     var searchPhoto = function(tag){
-        
-        console.log(tag);
+    
         var url= "https://api.instagram.com/v1/tags/" + tag + "/media/recent?callback=?&amp;client_id=d57bbd445f2e4d4685f8f3710ef9a394"
-        $.getJSON(url, viewData);
-           
-    
+       $.getJSON(url, viewData);
+        console.log(tag);
+        console.log(url);
+          
     };
-    
     
     var viewData = function(photos){
         
-        console.log(photos);
         $.each(photos.data, function(index, photo)
-        {
-            photo = "<div class='photoborder'>"
-            +"<img class='lowres'  src='" +photo.user.profile_picture+"' />" 
-            +"<img class='pic' src='" +photo.images.low_resolution.url +"'/>"
-            
-            +"<span class='likes'><strong>"+ photo.likes.count+"</strong></span>" +'</div>';
-            
-            
-         $('.container').append(photo);
-            
-         
-        
-        });
-        
-           
-    
+                {
+                    
+                        photo = viewTemplate(photo);
+                   
+                    
+
+                });
     };
     
+    var viewTemplate =function(photo){
+        var $phototemplate = $('#photo-template'),
+            $container =$('.container');
+        
+        var photo= {
+                    
+                        user: photo.user.profile_picture,
+                        lowres: photo.images.low_resolution.url,
+                        likes: photo.likes.count
+                    };
+                    
+        var template = Handlebars.compile($($phototemplate).html());
+                 $container.append(template(photo));
+        
+        
     
+    
+    };
+    var searchPhotoButton = function()
+    {
+     
+        var $phototemplate = $('#photo-template'),
+            $container = $('.container');
+        $('.search-sheet').submit(function(){
+      
+          
+            var inputList = $('.inputList');
+                    if ($.trim(inputList.val()) !== '')
+                    {
+                    var inputvalue = inputList.val();
+                       console.log(inputvalue);
+                        postaCard.search(inputvalue);
+                        console.log(inputvalue);
+                       
+                       
+                };
+               
+                    inputList.val('');
+                    return false;
+      
+  });
+           
 
+    };
+    
 
     // public API
     return {loadProp: init,
@@ -55,13 +83,11 @@ var postaCard = (function() {
  
 })();
 
-
 $(document).ready(function()
 {
     postaCard.search('santodomingo');
-    
-    
- 
+    postaCard.loadProp();
+  
 });
      
 
